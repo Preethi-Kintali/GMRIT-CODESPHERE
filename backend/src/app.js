f097422ee725ad5486f4e8882b0d9bcfc8df33a3
Dinesh Jammu <dineshjammu143@gmail.com>
2026-02-24 18:26:51 +0530

    Importing functions

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import { serve } from "inngest/express";
 
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
-import { functions } from "./lib/inngest.js";
+import { inngest, functions } from "./lib/inngest.js";
 
 const PORT = ENV.PORT;
 const app = express();
@@ -15,7 +15,7 @@ const __dirname = path.resolve();
 app.use(express.json());
 // credentials: true meaning?? => server allows a browser to include cookies on request
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-app.use("/api/inngest", serve({ client: inngest}, functions));
+app.use("/api/inngest", serve({ client: inngest, functions}));
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "api is up and running" });
